f5afb4d5166e2024eabbad1efe70a04e6548fc25
Brian Egan <brianjoseph.egan@gmail.com>
2023-09-03 18:49:40 -0700

    Add ITestOutputHelper
    Add logging of serialized JSON in unit test class to help with development.

--- DataSetConverter.Tests/DataSetConverterTests.cs
+++ DataSetConverter.Tests/DataSetConverterTests.cs
@@ -2,22 +2,31 @@ using AutoFixture;
 using FluentAssertions;
 using Newtonsoft.Json;
 using System.Data;
+using Xunit.Abstractions;
 
 namespace DataSetConverter.Tests
 {
     public class DataSetConverterTests
     {
+        private readonly ITestOutputHelper _output;
         private readonly Fixture _fixture = new();
         private readonly DataSetConverter _converter = new();
+
+        public DataSetConverterTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
 
         [Fact]
         public void SerializeDeserialize_Should_SetDataSetName()
         {
             var expected = new DataSet(dataSetName: _fixture.Create<string>());
 
-            var actual = JsonConvert.DeserializeObject<DataSet>(
-                JsonConvert.SerializeObject(expected, _converter),
-                _converter);
+            var json = JsonConvert.SerializeObject(expected, _converter);
+
+            _output.WriteLine($"JSON: {json}");
+
+            var actual = JsonConvert.DeserializeObject<DataSet>(json, _converter);
 
             actual.DataSetName.Should().Be(expected.DataSetName);
         }
